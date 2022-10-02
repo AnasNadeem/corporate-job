59983708f38bf165d76e647f5d9b2474ec941e02
Anas Nadeem <anas5678go@gmail.com>
2022-10-02 20:47:45 +0530

    Fixed IsCorporate and IsCorporateJobOwner permissions

--- jobapp/permissions.py
+++ jobapp/permissions.py
@@ -4,24 +4,17 @@ from jobapp.models import Corporate
 
 class IsCorporate(IsAuthenticated):
 
-    def has_permission(self, request, view):
+    def has_object_permission(self, request, view, obj):
         corporate = Corporate.objects.filter(user=request.user).first()
         if not corporate:
             return False
-        return True
-
-    def has_object_permission(self, request, view, obj):
         return obj.user == request.user
 
 
 class IsCorporateJobOwner(IsAuthenticated):
 
-    def has_permission(self, request, view):
+    def has_object_permission(self, request, view, obj):
         corporate = Corporate.objects.filter(user=request.user).first()
         if not corporate:
             return False
-
-        return bool(request.user and request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
         return obj.corporate.user == request.user
